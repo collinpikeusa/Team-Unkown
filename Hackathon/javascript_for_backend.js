37e76957155f34e84cb15d80f1178eb8e82ea177
Collin <collinpikeusa@gmail.com>
2017-02-04 09:20:46 -0600

    Check

--- Hackathon/javascript_for_backend.js
+++ Hackathon/javascript_for_backend.js
@@ -1,5 +1,5 @@
 https://api.nasa.gov/planetary/apod?api_key=n1nWvQETbmWAI9M7H9spub7FYeDe51Sf27ishYok
-
+var solConversion = 970587024380887;
 function solConverter(month, day, year) {
-    return 5 * 2
+    return 
 }
